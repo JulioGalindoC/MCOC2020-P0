f23ed795d672130b5d236f771ad71ace89cbe240
JulioGalindoC <69281523+JulioGalindoC@users.noreply.github.com>
2020-08-12 22:24:06 -0400

    Update timing_inv_caso_3_half.py

--- timing_inv_caso_3_half.py
+++ timing_inv_caso_3_half.py
@@ -57,7 +57,7 @@ for i in range(num_runs) :
         
         dt = t2 - t1
         
-        size = 2 * (i**2) * 26   # 2 Matrices, i^2, 26 bits por cada numero float
+        size = 1 * (i**2) * 26   # 1 Matriz, i^2, 26 bits por cada numero float
         
         dts.append(dt)
         mem.append(size)
